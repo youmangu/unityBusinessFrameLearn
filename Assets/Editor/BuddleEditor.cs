299fdec6ab3fb0b0ce35309420336ada3e70660e
youmangu <873211279@qq.com>
2020-07-28 13:25:57 +0800

    -[assets]打包AB包1

--- Assets/Editor/BuddleEditor.cs
+++ Assets/Editor/BuddleEditor.cs
@@ -7,18 +7,43 @@ public class BuddleEditor
 {
     private static string ABCONFIGPATH = "Assets/Editor/ABConfig.asset";
 
+    // key 是AB包的名字，value是路径
+    private static Dictionary<string, string> m_AllFileDir = new Dictionary<string, string>();
+
     [MenuItem("Tools/打包")]
     public static void Build()
     {
+        m_AllFileDir.Clear();
         ABConfig abConfig = AssetDatabase.LoadAssetAtPath<ABConfig>(ABCONFIGPATH);
 
-        foreach (var str in abConfig.m_AllPrefabPath)
-            Debug.Log(str);
-
-        foreach (var str in abConfig.m_AllFileDirAB)
+        foreach (ABConfig.FileDirABName fileDir in abConfig.m_AllFileDirAB)
         {
-            Debug.Log(str.ABName + "    " + str.Path);
+            if (m_AllFileDir.ContainsKey(fileDir.ABName))
+            {
+                Debug.LogError("已经包含了相同的文件名:" + fileDir.ABName + ", 请检查");
+            }
+            else
+            {
+                m_AllFileDir.Add(fileDir.ABName, fileDir.Path);
+            }
         }
+
+        // 返回的是 guid 数组
+        string []str = AssetDatabase.FindAssets("t:Prefab", abConfig.m_AllPrefabPath.ToArray());
+
+        for (int i = 0; i < str.Length; i++)
+        {
+            // 根据GUID获取到路径
+            string path = AssetDatabase.GUIDToAssetPath(str[i]);
+            // 显示进度条
+            EditorUtility.DisplayProgressBar("查找Prafab", "Prefab:" + path, i * 1.0f / str.Length);
+
+        }
+
+        EditorUtility.ClearProgressBar();
+
+
+
 
         Debug.Log("打包");
         //BuildPipeline.BuildAssetBundles(Application.streamingAssetsPath, BuildAssetBundleOptions.ChunkBasedCompression, EditorUserBuildSettings.activeBuildTarget);
