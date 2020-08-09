6fcc472a05b07c49e29da6f5b1417cc674821600
youmangu <873211279@qq.com>
2020-08-10 07:52:03 +0800

    -[ab]ab 打包添加过滤

--- Assets/Editor/BuddleEditor.cs
+++ Assets/Editor/BuddleEditor.cs
@@ -10,6 +10,12 @@ public class BuddleEditor
     // key 是AB包的名字，value是路径
     private static Dictionary<string, string> m_AllFileDir = new Dictionary<string, string>();
 
+    // 过滤ab包
+    private static List<string> m_AllFileAB = new List<string>();
+
+    // 单个prefab的ab包
+    private static Dictionary<string, List<string>> m_AllPrefabDir = new Dictionary<string, List<string>>(); 
+
     [MenuItem("Tools/打包")]
     public static void Build()
     {
@@ -25,18 +31,47 @@ public class BuddleEditor
             else
             {
                 m_AllFileDir.Add(fileDir.ABName, fileDir.Path);
+                m_AllFileAB.Add(fileDir.ABName);
             }
         }
 
         // 返回的是 guid 数组
         string []str = AssetDatabase.FindAssets("t:Prefab", abConfig.m_AllPrefabPath.ToArray());
 
-        for (int i = 0; i < str.Length; i++)
+        for (int i = 0; i < str.Length; i++)  
         {
             // 根据GUID获取到路径
             string path = AssetDatabase.GUIDToAssetPath(str[i]);
             // 显示进度条
             EditorUtility.DisplayProgressBar("查找Prafab", "Prefab:" + path, i * 1.0f / str.Length);
+
+            if (!ContainAllFileAB(path))
+            {
+                GameObject obj = AssetDatabase.LoadAssetAtPath<GameObject>(path);
+                string []depencies = AssetDatabase.GetDependencies(path);
+                List<string> allDepencies = new List<string>();
+                for (int j = 0; j < depencies.Length; j++)
+                {
+                    Debug.Log(depencies[j]);
+                    if (!ContainAllFileAB(depencies[i]) && depencies[i].EndsWith(".cs"))
+                    {
+                        m_AllFileAB.Add(depencies[i]);
+                        allDepencies.Add(depencies[i]);
+
+                    }
+                }
+
+                if (m_AllPrefabDir.ContainsKey(obj.name))
+                {
+                    Debug.LogError("存在相同名字的prefab，名字：" + obj.name);
+                }
+                else { 
+                    m_AllPrefabDir.Add(obj.name, allDepencies);
+                }
+
+            }
+
+
 
         }
 
@@ -48,5 +83,16 @@ public class BuddleEditor
         Debug.Log("打包");
         //BuildPipeline.BuildAssetBundles(Application.streamingAssetsPath, BuildAssetBundleOptions.ChunkBasedCompression, EditorUserBuildSettings.activeBuildTarget);
         //AssetDatabase.Refresh();
+    }
+
+    static bool ContainAllFileAB(string path)
+    {
+        for (int i = 0; i < m_AllFileAB.Count; i++)
+        {
+            if (path == m_AllFileAB[i] || path.Contains(m_AllFileAB[i]))
+                return true;
+        }
+
+        return false;
     }
 }
