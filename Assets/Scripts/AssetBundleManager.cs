95cb0c1583dfd3d7ca0acf2858022ddb69a21666
youmangu <873211279@qq.com>
2021-03-26 13:02:22 +0800

    -[AB]资源加载、查找和释放

--- Assets/Scripts/AssetBundleManager.cs
+++ Assets/Scripts/AssetBundleManager.cs
@@ -7,7 +7,11 @@ using UnityEngine;
 public class AssetBundleManager : Singleton<AssetBundleManager>
 {
     // 资源关系依赖配置表， 可以根据crc来找到对应资源块
-    protected static Dictionary<uint, AssetBundleItem> m_ResourceItemDic = new Dictionary<uint, AssetBundleItem>();
+    protected static Dictionary<uint, ResourceItem> m_ResourceItemDic = new Dictionary<uint, ResourceItem>();
+    // 储存AssetBundleItem， key为路径的crc
+    private static Dictionary<uint, AssetBundleItem> m_AssetBundleDic = new Dictionary<uint, AssetBundleItem>();
+    // AssetBundleItem 对象池
+    private static ClassObjectPool<AssetBundleItem> m_AssetBundleItemPool = new ClassObjectPool<AssetBundleItem>(500);
 
     /// <summary>
     /// 加载AB配置表
@@ -34,7 +38,7 @@ public class AssetBundleManager : Singleton<AssetBundleManager>
         m_ResourceItemDic.Clear();
         foreach (ABBase abBase in config.ABList)
         {
-            AssetBundleItem abItem = new AssetBundleItem();
+            ResourceItem abItem = new ResourceItem();
             abItem.m_Crc = abBase.Crc;
             abItem.m_AssetName = abBase.ABName;
             abItem.m_AssetBundleName = abBase.AssetName;
@@ -52,10 +56,132 @@ public class AssetBundleManager : Singleton<AssetBundleManager>
 
         return true;
     }
+
+    /// <summary>
+    /// 根据路径Crc加载中间类 ResourceItem
+    /// </summary>
+    /// <param name="crc"></param>
+    /// <returns></returns>
+    public ResourceItem LoadResourceAssetBundle(uint crc)
+    {
+        ResourceItem item = null;
+        if (m_ResourceItemDic.TryGetValue(crc, out item) || item == null)
+        {
+            Debug.LogError("LoadResourceAssetBundle error: cannot find crc {0} in AssetBundleConfig", crc);
+            return item;
+        }
+
+        if (item.m_AssetBundle != null)
+        {
+            return item;
+        }
+
+        item.m_AssetBundle = LoadAssetBundle(item.m_AssetBundleName);
+
+        // 加载依赖
+        if (item.m_DependAssetBundle != null)
+        {
+            for (int i = 0; i < item.m_DependAssetBundle.Count; i++)
+            {
+                LoadAssetBundle(item.m_DependAssetBundle[i]);
+            }
+        }
+
+        return item;
+    }
+
+    private AssetBundle LoadAssetBundle(string name)
+    {
+        AssetBundleItem item = null;
+        uint crc = CRC32.GetCRC32(name);
+        if (!m_AssetBundleDic.TryGetValue(crc, out item))
+        {
+            AssetBundle assetBundle = null;
+            string fullPath = Application.streamingAssetsPath + "/" + name;
+            if (File.Exists(fullPath))
+            {
+                assetBundle = AssetBundle.LoadFromFile(fullPath);
+            }
+
+            if (assetBundle == null)
+            {
+                Debug.LogError("Load AssetBundle error: " + fullPath);
+            }
+
+            item = m_AssetBundleItemPool.Spawn(true);
+            item.assetBundle = assetBundle;
+            item.RefCount++;
+            m_AssetBundleDic.Add(crc, item);
+        }
+        else
+        {
+            item.RefCount++;
+        }
+
+        return item.assetBundle;
+;    }
+
+    /// <summary>
+    /// 释放资源
+    /// </summary>
+    /// <param name="item"></param>
+    public void ReleaseAsset(ResourceItem item)
+    {
+        if (item == null)
+            return;
+
+        if (item.m_DependAssetBundle != null && item.m_DependAssetBundle.Count > 0)
+        {
+            for (int i = 0; i < item.m_DependAssetBundle.Count; i++)
+            {
+                UnLoadAssetBundle(item.m_DependAssetBundle[i]);
+            }
+        }
+        UnLoadAssetBundle(item.m_AssetBundleName);
+    }
+
+    private void UnLoadAssetBundle(string name)
+    {
+        AssetBundleItem item = null;
+        uint crc = CRC32.GetCRC32(name);
+        if(m_AssetBundleDic.TryGetValue(crc, out item) && (item != null))
+        {
+            item.RefCount--;
+            if (item.RefCount <= 0 && item.assetBundle != null)
+            {
+                item.assetBundle.Unload(true);
+                item.Reset();
+                m_AssetBundleItemPool.Recycle(item);
+                m_AssetBundleDic.Remove(crc);
+            }
+        }
+    }
+
+    /// <summary>
+    /// 根据crc 查找 ResourceItem
+    /// </summary>
+    /// <param name="crc"></param>
+    /// <returns></returns>
+    public ResourceItem FindResourceItem(uint crc)
+    {
+        return m_ResourceItemDic[crc];
+    }
+}
+
+public class AssetBundleItem
+{
+    public AssetBundle assetBundle = null;
+    public int RefCount;
+
+    public void Reset()
+    {
+        assetBundle = null;
+        RefCount = 0;
+    }
 }
 
 
-public class AssetBundleItem
+public class ResourceItem
 {
     // 资源路径的CRC
     public uint m_Crc = 0;
