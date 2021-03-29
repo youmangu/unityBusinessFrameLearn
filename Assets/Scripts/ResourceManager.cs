639cc42dfcdda494a9f586a9e4c858ce590f75ca
youmangu <873211279@qq.com>
2021-03-29 10:56:23 +0800

    -[DoubleLinkedList] double link list

--- Assets/Scripts/ResourceManager.cs
+++ Assets/Scripts/ResourceManager.cs
@@ -6,3 +6,156 @@ public class ResourceManager : Singleton<ResourceManager>
 {
 
 }
+
+//双向链表结构节点
+public class DoubleLinkedListNode<T> where T : class, new()
+{
+    // 前一个节点
+    public DoubleLinkedListNode<T> prev = null;
+    // 后一个节点
+    public DoubleLinkedListNode<T> next = null;
+    // 当前节点
+    public T t = null;
+}
+
+//双向链表结构
+public class DoubleLinkedList<T> where T : class, new()
+{
+    // 表头
+    public DoubleLinkedListNode<T> Head = null;
+    // 表尾
+    public DoubleLinkedListNode<T> Tail = null;
+    // 双向链表结构类对象池
+    protected ClassObjectPool<DoubleLinkedListNode<T>> m_DoubleLinkedNodePool = ObjectManager.Instance.GetOrCreateClassPool<DoubleLinkedListNode<T>>(500);
+    // 个数
+    protected int m_Count = 0;
+    public int Count
+    {
+        get { return m_Count; }
+    }
+
+    /// <summary>
+    /// 添加到头部节点
+    /// </summary>
+    /// <param name="t"></param>
+    /// <returns></returns>
+    public DoubleLinkedListNode<T> AddToHeader(T t)
+    {
+        DoubleLinkedListNode<T> plist = m_DoubleLinkedNodePool.Spawn(true);
+        plist.prev = null;
+        plist.next = null;
+        plist.t = t;
+        return AddToHeader(plist);
+    }
+
+    /// <summary>
+    /// 添加到头部
+    /// </summary>
+    /// <param name="pNode"></param>
+    /// <returns></returns>
+    public DoubleLinkedListNode<T> AddToHeader(DoubleLinkedListNode<T> pNode)
+    {
+        if (pNode == null)
+            return null;
+        pNode.prev = null;
+        if (Head == null)
+        {
+            Head = Tail = pNode;
+        }
+        else
+        {
+            pNode.next = Head;
+            Head.prev = pNode;
+            Head = pNode;
+        }
+
+        m_Count++;
+        return Head;
+            
+    }
+
+    /// <summary>
+    /// 添加节点到尾部
+    /// </summary>
+    /// <param name="t"></param>
+    /// <returns></returns>
+    public DoubleLinkedListNode<T> AddToTail(T t)
+    {
+        DoubleLinkedListNode<T> pNode = m_DoubleLinkedNodePool.Spawn(true);
+        pNode.next = null;
+        pNode.prev = null;
+        pNode.t = t;
+        return AddToTail(pNode);
+    }
+
+    /// <summary>
+    /// 添加节点到尾部
+    /// </summary>
+    /// <param name="pNode"></param>
+    /// <returns></returns>
+    public DoubleLinkedListNode<T> AddToTail(DoubleLinkedListNode<T> pNode)
+    {
+        if (pNode == null) return null;
+
+        pNode.next = null;
+        if (Tail == null)
+        {
+            Head = Tail = pNode;
+        }
+        else
+        {
+            pNode.prev = Tail;
+            Tail.next = pNode;
+            Tail = pNode;
+        }
+
+        m_Count++;
+        return Tail;
+    }
+
+    public void RemoveNode(DoubleLinkedListNode<T> pNode)
+    {
+        if (pNode == null)
+            return;
+        if (Head == pNode)
+            Head = pNode.next;
+        if (Tail == pNode)
+            Tail = pNode.prev;
+        if (pNode.next != null)
+            pNode.next.prev = pNode.prev;
+
+        pNode.next = pNode.prev = null;
+        pNode.t = null;
+        m_DoubleLinkedNodePool.Recycle(pNode);
+        m_Count--;
+    }
+
+    public void MoveToHead(DoubleLinkedListNode<T> pNode)
+    {
+        if (pNode == null || pNode == Head)
+            return;
+
+        if (pNode.prev == null && pNode.next == null)
+            return;
+
+        if (pNode == Tail)
+            Tail = pNode.prev;
+
+        if (pNode.prev != null)
+            pNode.prev.next = pNode.next;
+
+        if (pNode.next != null)
+            pNode.next.prev = pNode.prev;
+
+        pNode.prev = null;
+        pNode.next = Head;
+        Head.prev = pNode;
+        Head = pNode;
+        if (Tail == null)
+        {
+            Tail = Head;
+        }
+    }
+
+
+}
